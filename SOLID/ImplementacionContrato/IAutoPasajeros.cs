431df1f73f33372c51d22b978989dcbc274eed11
mauricio.bello <mauricio.bello@WIN-DESARROLLO>
2021-07-13 14:09:44 -0500

    IAutoPasajeros

---
+++ SOLID/ImplementacionContrato/IAutoPasajeros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLID.ImplementacionContrato
+{
+    public interface IAutoPasajeros
+    {
+        public int getTrasportarPasajero();
+    }
+}
